a745a493b83d08e99b97006f3c2142f62b710de4
Alianza <janwillemvanbremen@live.nl>
2021-09-30 02:08:10 +0200

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './dist/**/*.html',
+    './src/**/*.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: 'media', // or 'media' or 'class'
