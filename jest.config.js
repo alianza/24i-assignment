bcea9fcf994ac636cbac8db272a0b280d49b1555
Alianza <janwillemvanbremen@live.nl>
2021-09-30 17:55:57 +0200

    Added jest config and tests

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// jest.config.js
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: false,
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
+  testEnvironment: "jsdom",
+  transformIgnorePatterns: ["node_modules/(?!(http|node-fetch|fetch-blob)/)"],
+  setupFilesAfterEnv: ["./jest/setupTests.js"],
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+};
+
+module.exports = config;
