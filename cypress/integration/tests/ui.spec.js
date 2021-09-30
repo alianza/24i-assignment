d06e638feca211bcfec01b632513eb3a5bbd93ce
Alianza <janwillemvanbremen@live.nl>
2021-09-30 15:15:48 +0200

    Update ui cypress test

--- cypress/integration/tests/ui.spec.js
+++ cypress/integration/tests/ui.spec.js
@@ -7,8 +7,14 @@ describe('The user interface', () => {
         // Scrolls to bottom to make scrollToTop button appear, presses the button and asserts page scrolled up to top
         console.log('Loaded base url');
 
+        cy.waitUntil(() => cy.get('#loader').should('be.hidden'));
+
         cy.scrollTo('bottom');
-        cy.findAllByText('↑').click();
+
+        cy.waitUntil(() => cy.findByText('↑').should('be.visible'));
+
+        cy.findByText('↑').click();
+
         cy.window().then(win => {
             expect(win.window.screenTop).to.equal(0);
         });
